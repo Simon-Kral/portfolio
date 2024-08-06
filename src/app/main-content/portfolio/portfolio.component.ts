384f879b401c5674bce4f8654bf1dc02f5d91b29
Simon Kral <simonkral92@gmail.com>
2024-08-06 16:03:49 +0200

    added da-bubble

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -4,41 +4,45 @@ import { NgForOf } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [
-    ProjectsComponent,
-    NgForOf,
-    TranslateModule
-  ],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+	selector: 'app-portfolio',
+	standalone: true,
+	imports: [ProjectsComponent, NgForOf, TranslateModule],
+	templateUrl: './portfolio.component.html',
+	styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-  projectList = [
-    {
-      title: "Join",
-      path: "join",
-      type: "JavaScript | HTML | CSS",
-      img: "./assets/img/join.png",
-      url: "http://join.simon-kral.de/",
-      github: "https://www.github.com/Simon-Kral/join",
-    },
-    {
-      title: "El Pollo Loco",
-      path: "elPolloLoco",
-      type: "JavaScript | HTML | CSS",
-      img: "./assets/img/el_pollo_loco.png",
-      url: "http://el-pollo-loco.simon-kral.de/",
-      github: "https://github.com/Simon-Kral/el-pollo-loco",
-    },
-    {
-      title: "Pokédex",
-      path: "pokedex",
-      type: "JavaScript | HTML | CSS | Api",
-      img: "./assets/img/pokedex.png",
-      url: "http://pokedex.simon-kral.de/",
-      github: "https://github.com/Simon-Kral/pokedex",
-    },
-  ]
+	projectList = [
+		{
+			title: 'DA-Bubble',
+			path: 'da-bubble',
+			type: 'Angular | Typescript | HTML | SCSS | Firebase',
+			img: './assets/img/da-bubble.png',
+			url: 'http://da-bubble.simon-kral.de/',
+			github: 'https://github.com/Simon-Kral/da-bubble',
+		},
+		{
+			title: 'Join',
+			path: 'join',
+			type: 'JavaScript | HTML | CSS',
+			img: './assets/img/join.png',
+			url: 'http://join.simon-kral.de/',
+			github: 'https://www.github.com/Simon-Kral/join',
+		},
+		{
+			title: 'El Pollo Loco',
+			path: 'elPolloLoco',
+			type: 'JavaScript | HTML | CSS',
+			img: './assets/img/el_pollo_loco.png',
+			url: 'http://el-pollo-loco.simon-kral.de/',
+			github: 'https://github.com/Simon-Kral/el-pollo-loco',
+		},
+		// {
+		//   title: "Pokédex",
+		//   path: "pokedex",
+		//   type: "JavaScript | HTML | CSS | Api",
+		//   img: "./assets/img/pokedex.png",
+		//   url: "http://pokedex.simon-kral.de/",
+		//   github: "https://github.com/Simon-Kral/pokedex",
+		// },
+	];
 }
